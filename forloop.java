a64400c6ed733eaf826d0081ae6bd5ba836f7045
Phxm11 <106417608+Phxm11@users.noreply.github.com>
2025-01-04 23:50:56 +0700

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,10 @@
+public class forloop {
+    public static void main(String[] args) {
+        int x = 0;
+        for (int i = 0; i < 100; i++) {
+            x += 1;
+
+            System.out.println("" + x);
+        }
+    }
+}
